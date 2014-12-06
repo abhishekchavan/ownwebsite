715ea8335c146efd00bf17e5e5616ca7c80b6406
abhishekchavan <abhishek.chavan@pearson.com>
2014-12-05 23:00:41 -0800

    Changes

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abhishekchavanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the abhishekchavanApp
+ */
+angular.module('abhishekchavanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
